1402fc165fa2ea6e75cc5612de68cf49a1f9be27
omartabbaa <omartabbaa0@gmail.com>
2024-09-09 15:50:50 +0200

    Initial commit of Spring Boot project

---
+++ src/test/java/nl/Tech/It/Easy/Tech/It/Easy/TechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.Tech.It.Easy.Tech.It.Easy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
